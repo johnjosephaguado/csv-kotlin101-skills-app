b002066afdae60e552561b30e3f780ec1366fae7
johnaguado <john.joseph.aguado@alaskaair.com>
2023-08-26 07:19:44 +0800

    Update levels when user clicks on added skills

--- app/src/main/java/com/cognizant/app/skills/ViewProfileActivity.kt
+++ app/src/main/java/com/cognizant/app/skills/ViewProfileActivity.kt
@@ -3,13 +3,16 @@ package com.cognizant.app.skills
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.cognizant.app.skills.LevelsFragment.LevelDialogListener
 import com.cognizant.app.skills.adapter.SkillsAdapter
 import com.cognizant.app.skills.data.ConsultantSkillsResponse
 import com.cognizant.app.skills.data.LevelsResponse
+import com.cognizant.app.skills.data.SkillsTypeResponse
 import com.cognizant.app.skills.data.api.ConsultantInterface
 import com.cognizant.app.skills.data.api.LevelsInterface
 import com.cognizant.app.skills.data.api.RetrofitClient
@@ -17,22 +20,24 @@ import com.cognizant.app.skills.databinding.ActivityProfileBinding
 import kotlinx.coroutines.launch
 import java.util.ArrayList
 
-class ViewProfileActivity: AppCompatActivity() {
+class ViewProfileActivity
+: AppCompatActivity(), LevelDialogListener {
 
     private lateinit var binding: ActivityProfileBinding
     private lateinit var name: TextView
     private lateinit var position: TextView
     private lateinit var email: TextView
     private lateinit var btnAddSkills: Button
-    private var allowEditing = false
+    private var selectedSkill: ConsultantSkillsResponse? = null
     private lateinit var levels: List<LevelsResponse>
     private var skills: List<ConsultantSkillsResponse> = listOf()
+    private var consultantId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val consultantId = intent.getIntExtra(CONSULTANT_ID, 0)
+        consultantId = intent.getIntExtra(CONSULTANT_ID, 0)
         name = findViewById(R.id.name)
         position = findViewById(R.id.position)
         email = findViewById(R.id.email)
@@ -75,10 +80,15 @@ class ViewProfileActivity: AppCompatActivity() {
             var response = consultantService.getConsultSkillsById(consultantId)
             if(response.isSuccessful) {
                 response.body()?.let {
-                    var consultantAdapter = SkillsAdapter(it)
+                    var skillsAdapter = SkillsAdapter(it)
+                    skillsAdapter.setOnClickListener { skill ->
+                        selectedSkill = skill
+                        val levelDialog = LevelsFragment(levels, this@ViewProfileActivity)
+                        levelDialog.show(supportFragmentManager, "test")
+                    }
                     skills = it
                     binding.listview.apply {
-                        adapter = consultantAdapter
+                        adapter = skillsAdapter
                         setHasFixedSize(true)
                     }
                 }
@@ -94,9 +104,34 @@ class ViewProfileActivity: AppCompatActivity() {
             if(response.isSuccessful) {
                 response.body()?.let {
                     levels = it
+                }
+            }
+        }
+    }
 
-                    val levelDialog = LevelsFragment(levels, this@ViewProfileActivity)
-                    levelDialog.show(supportFragmentManager, "test")
+    override fun onSelect(level: LevelsResponse) {
+        Log.d("View Profile select", "${level.description}")
+        updateSkill(level)
+    }
+
+    private fun updateSkill(level: LevelsResponse) {
+        val retrofitClient = RetrofitClient.getInstance()
+        val consultantService = retrofitClient.create(ConsultantInterface::class.java)
+        lifecycleScope.launch {
+            val skillsResponse = ConsultantSkillsResponse(
+                consultantId,
+                selectedSkill?.skillId ?: 0,
+                selectedSkill?.skillTypeId ?: 0,
+                selectedSkill?.skillName ?: "",
+                selectedSkill?.years ?: 0,
+                level.id,
+                level.description
+            )
+
+            val response = consultantService.addSkill(skillsResponse)
+            if(response.isSuccessful) {
+                response.body()?.let {
+                    viewConsultantSkills(consultantId)
                 }
             }
         }
--- app/src/main/java/com/cognizant/app/skills/data/api/ConsultantInterface.kt
+++ app/src/main/java/com/cognizant/app/skills/data/api/ConsultantInterface.kt
@@ -5,6 +5,7 @@ import com.cognizant.app.skills.data.ConsultantSkillsResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
@@ -24,4 +25,7 @@ interface ConsultantInterface {
 
     @PUT("/api/consultant/skill/add")
     suspend fun addSkill(@Body skill: ConsultantSkillsResponse): Response<ConsultantSkillsResponse>
+
+    @PATCH("/api/consultant/skill/update")
+    suspend fun updateLevel(@Body skill: ConsultantSkillsResponse): Response<ConsultantSkillsResponse>
 }
