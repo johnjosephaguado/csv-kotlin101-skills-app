7bd2f9baf179e2dd697d04a572a5454d0a4555b0
Michelle Tedria <michellemabel.tedria@cognizant.com>
2023-08-29 14:51:56 +0800

    Login screen simple validation

--- app/src/main/java/com/cognizant/app/skills/LoginActivity.kt
+++ app/src/main/java/com/cognizant/app/skills/LoginActivity.kt
@@ -1,7 +1,11 @@
 package com.cognizant.app.skills
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.cognizant.app.skills.SearchListActivity
 import com.cognizant.app.skills.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -13,5 +17,25 @@ class LoginActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        with(binding) {
+
+            btLogin.setOnClickListener {
+                if (TextUtils.isEmpty(etUsername.text.toString())) {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please input your Cognizant ID",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    if (TextUtils.isEmpty(etPassword.text.toString())) {
+                        Toast.makeText(this@LoginActivity, "Invalid Password", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                } else {
+                    val intent = Intent(this@LoginActivity, SearchListActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
