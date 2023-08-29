06e910710951b0687c384eabf02a8656e5ab55dd
Renzy Ortega <devortegarenzy@gmail.com>
2023-08-29 13:28:01 +0800

    feat: improve ui layout in skills, use of skill logos

--- app/src/main/java/com/cognizant/app/skills/adapter/SkillsAdapter.kt
+++ app/src/main/java/com/cognizant/app/skills/adapter/SkillsAdapter.kt
@@ -1,22 +1,25 @@
 package com.cognizant.app.skills.adapter
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.NonNull
-import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.cognizant.app.skills.R
-import com.cognizant.app.skills.data.ConsultantResponse
 import com.cognizant.app.skills.data.ConsultantSkillsResponse
-import com.cognizant.app.skills.model.Consultant
 
 class SkillsAdapter(private val skills: List<ConsultantSkillsResponse>): RecyclerView.Adapter<SkillsAdapter.ViewHolder>() {
 
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        var logoView: ImageView = view.findViewById(R.id.logo)
         var name: TextView = view.findViewById(R.id.name)
+        var level: TextView = view.findViewById(R.id.level)
+        var context: Context = view.context
     }
 
     @NonNull
@@ -27,21 +30,29 @@ class SkillsAdapter(private val skills: List<ConsultantSkillsResponse>): Recycle
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         var item = skills[position]
-
-        val level = item.levelName.let {
-            if (it.isNullOrEmpty()) {
-                ""
-            } else {
-                ": $it"
-            }
+        var drawableName = item.skillName!!.lowercase().replace(" ", "").trim()
+        val drawable: Drawable? = loadDrawableByName(holder.context, drawableName)
+        if(drawable != null) {
+            holder.logoView!!.setImageDrawable(drawable)
         }
 
-        holder.name!!.text = item.skillName + level
+        holder.name!!.text = item.skillName
+        holder.level!!.text = item.levelName
 
         holder.itemView.apply {
             setOnClickListener {
                 onClickListener?.let { it(item) }
             }
+        }
+    }
+
+    private fun loadDrawableByName(context: Context, drawableName: String): Drawable? {
+        val resourceId = context.resources.getIdentifier(drawableName, "drawable", context.packageName)
+        return if (resourceId != 0) {
+            ContextCompat.getDrawable(context, resourceId)
+        }
+        else {
+            return null
         }
     }
 
--- app/src/main/java/com/cognizant/app/skills/adapter/SkillsTypeAdapter.kt
+++ app/src/main/java/com/cognizant/app/skills/adapter/SkillsTypeAdapter.kt
@@ -1,11 +1,15 @@
 package com.cognizant.app.skills.adapter
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.NonNull
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.cognizant.app.skills.R
 import com.cognizant.app.skills.data.SkillsTypeResponse
@@ -15,7 +19,9 @@ class SkillsTypeAdapter(
     private val skillsType: List<SkillsTypeResponse>
 ) : RecyclerView.Adapter<SkillsTypeAdapter.ViewHolder>() {
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        var logoView: ImageView = view.findViewById(R.id.logo)
         var name: TextView = view.findViewById(R.id.name)
+        var context: Context = view.context
     }
 
     @NonNull
@@ -25,8 +31,22 @@ class SkillsTypeAdapter(
         return ViewHolder(view)
     }
 
+    private fun loadDrawableByName(context: Context, drawableName: String): Drawable? {
+        val resourceId = context.resources.getIdentifier(drawableName, "drawable", context.packageName)
+        return if (resourceId != 0) {
+            ContextCompat.getDrawable(context, resourceId)
+        }
+        else {
+            return null
+        }
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         var item = skillsType[position]
+        val drawable: Drawable? = loadDrawableByName(holder.context, item.description!!.lowercase())
+        if(drawable != null) {
+            holder.logoView!!.setImageDrawable(drawable)
+        }
         holder.name!!.text = item.description
         holder.name.tag = position
 
